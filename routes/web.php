ec5df4ef2d398c2e8fc815dee221db704f81066a
Chihab Derghal <chihabderghal1@gmail.com>
2023-09-22 20:11:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    dd(Auth::user());
+
+})->middleware(['auth', 'verified']);
+
